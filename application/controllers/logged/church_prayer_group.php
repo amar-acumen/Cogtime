828badb799792bf763dd556bbe9f2e6e443e982e
arifh <arif.zisu@gmail.com>
2015-02-24 16:04:08 +0530

    generate_prayer_group_listing_AJAX

--- application/controllers/logged/church_prayer_group.php
+++ application/controllers/logged/church_prayer_group.php
@@ -1853,7 +1853,7 @@ class Church_prayer_group extends Base_controller
 			$data['church_admin'] = $this->church_new_model->get_church_admin_data($_SESSION['logged_church_id']);
 
 		$data['church_list'] =  $this->church_new_model->get_prayer_group_search_result_by_church($_SESSION['logged_church_id'],$logged_user_id,$order='dt_created_on',$direction='DESC',$s_where);
-		//pr($data['church_list']);
+		 pr($data['church_list']);
         # loading the view-part...
         $AJAX_VIEW_FILE = 'logged/church/search_prayer_group_listing_ajax.phtml';
         
