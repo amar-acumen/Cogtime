7cdaca45e40e37e70a338d54adbab269de9d1717
Sudipta <webguruslogic@gmail.com>
2015-02-25 22:19:30 +0530

    CSS n JS Performance minify Tool

--- application/controllers/module_generate.php
+++ application/controllers/module_generate.php
@@ -74,6 +74,7 @@ class Module_generate extends CI_Controller {
                     'assets/js/contrib/jquery-ui-1.10.4.custom.min.js',
                     //'assets/js/contrib/gmap3.min.js',*/
                     'js/jquery-1.7.2.js',
+                    'js/jquery.cookie.js',
                     'js/jquery/ui/jquery.blockUI.js',
                     'js/jquery/ui/jquery.ui.core.js',
                     'js/frontend/utilities.js',
