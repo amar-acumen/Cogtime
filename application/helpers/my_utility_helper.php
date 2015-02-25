dd91f3684aa306f3d95c64781c5e6e67615cb72b
arifh <arif.zisu@gmail.com>
2015-02-25 12:59:00 +0530

    86

--- application/helpers/my_utility_helper.php
+++ application/helpers/my_utility_helper.php
@@ -5132,7 +5132,7 @@ function get_prayer_group_post_count_by_grp_id($gid){
    $ci = & get_instance();
     $sql = $ci->db->query('select count(*) as count from cg_church_prayer_group_post where i_prayer_group_id="' . $gid . '" ');
     //echo $sql;
-    echo $sql1 = $this->db->last_query();
+    echo $sql = $this->db->last_query();
     $res = $sql->result_array();
     if ($res['0']['count'] > 0) {
         return true;
