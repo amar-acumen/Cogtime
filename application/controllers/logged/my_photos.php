be08ae4ccf99b58b2c518549857673a0ee143916
sanhita-acumen <sanhita@acumensoft.info>
2015-02-26 09:50:27 +0530

    profile image

--- application/controllers/logged/my_photos.php
+++ application/controllers/logged/my_photos.php
@@ -1343,7 +1343,7 @@ class My_photos extends Base_controller {
 
                 foreach ($result as $key => $val) {
 
-                    $profile_image_filename = get_profile_image($val['i_user_id'], 'thumb');
+                    $profile_image_filename = get_profile_image_of_user('thumb',$val['s_profile_photo'],$val['e_gender']);
                     $DESC = html_entity_decode(htmlspecialchars_decode($val['s_contents']), ENT_QUOTES, 'utf-8');
                     $profile_link = get_profile_url($val['i_user_id'], $val['s_profile_name']);
 
