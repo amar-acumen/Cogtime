b6e4b019273344ebe97e3cc9601ae37bb5437a08
arifh <arif.zisu@gmail.com>
2015-02-26 15:38:33 +0530

    chech data in user model

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -764,7 +764,8 @@ class Users_model extends Base_model implements InfModel {
             $this->db->trans_begin(); ///new
             $rs = $this->db->query($s_qry, $stmt_val);
             #echo $this->db->last_query();    
-
+              $res = $rs->result();
+              pr($res,1);
             if (is_array($rs->result())) { ///new
                 foreach ($rs->result() as $row) {
                     $ret_["id"] = $row->id; ////always integer 
