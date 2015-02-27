885e3906e135437025a93aea197fb80b40a2cece
arifh <arif.zisu@gmail.com>
2015-02-27 18:43:23 +0530

    member post

--- application/models/data_newsfeed_model.php
+++ application/models/data_newsfeed_model.php
@@ -390,7 +390,7 @@ $sql="select distinct c.* from cg_users u ,cg_user_newsfeeds c where 1 and c.i_o
 
 }
 else*/
-{$sql="select distinct c.* ,(SELECT count(*) FROM cg_user_newsfeed_comments RC WHERE RC.i_newsfeed_id = c.id) as total_comments from cg_users u ,cg_user_newsfeeds c ".$s_where." and c.i_owner_id='".$i_user_id."'and u.i_isdeleted='1' ORDER BY c.id DESC  limit ".$i_start_limit.",".$i_no_of_page;}
+{$sql="select distinct c.* ,CONCAT(u.s_first_name,' ',u.s_last_name) AS s_profile_name , (SELECT count(*) FROM cg_user_newsfeed_comments RC WHERE RC.i_newsfeed_id = c.id) as total_comments from cg_users u ,cg_user_newsfeeds c ".$s_where." and c.i_owner_id='".$i_user_id."'and u.i_isdeleted='1' ORDER BY c.id DESC  limit ".$i_start_limit.",".$i_no_of_page;}
 #echo $sql;
 $query=$this->db->query($sql);
 $result_arr=$query->result_array();
