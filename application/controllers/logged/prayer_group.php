e699f92e042ee9c70ad906c8d8932190be3c32ae
Sudipta <webguruslogic@gmail.com>
2015-02-27 14:46:33 +0530

    CSS n JS Performance minify Tool

--- application/controllers/logged/prayer_group.php
+++ application/controllers/logged/prayer_group.php
@@ -1594,14 +1594,14 @@ class Prayer_group extends Base_controller
             parent::_set_meta_keywords('');
         
             
-//            parent::_add_js_arr( array( 'js/ddsmoothmenu.js',
+            parent::_add_js_arr( array( //'js/ddsmoothmenu.js',
 //                                        'js/switch.js','js/animate-collapse.js',
 //                                        'js/lightbox.js',
 //                                        'js/jquery-ui-1.8.2.custom.min.js',
 //                                        'js/stepcarousel.js',
 //                                        'js/tab.js',
-//                                        'js/frontend/logged/holy_place/prayer_group.js',
-//                                        ));
+                                       'js/frontend/logged/holy_place/prayer_group.js',
+                                        ));
 //
 //            parent::_add_css_arr( array('css/jquery-ui-1.8.13.custom.css') );
           
