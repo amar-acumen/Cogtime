feff0e6f0a2be312b2b2d0beee7ef1745cbb6f52
Trisha <trisha.p@acumensoft.info>
2015-03-02 13:26:45 +0530

    writehead in error

--- nodejs/server.js
+++ nodejs/server.js
@@ -50,6 +50,7 @@ http.createServer(function (req, res) {
 					}
 					else 
 					{
+							res.writeHead(200, {'Content-Type': 'text/html'});
 							res.end('setUpdateStatus("error")');
 					}
 				});
