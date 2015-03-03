63f036d3d2032798f5a6501eaf17b78a86ef4b4d
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-03 12:13:24 +0430

    add member

--- application/models/church_new_model.php
+++ application/models/church_new_model.php
@@ -579,7 +579,7 @@ class Church_new_model extends Base_model
 	function get_church_invited_members($c_id,$i_start=0,$order_by='id DESC',$i_limit=0){
     		$limit = (is_numeric($i_start) && is_numeric($i_limit)) ? " Limit " . intval($i_start) . "," . intval($i_limit) : '';
         	$order_by =  " ORDER BY {$order_by}" ;
-            $sql = 'select * from cg_church_member_invitation WHERE church_id = "'.$c_id.'" AND '.$order_by.' '.$limit.'';
+            $sql = 'select * from cg_church_member_invitation WHERE church_id = "'.$c_id.'" '.$order_by.' '.$limit.'';
 				//echo $sql;
             $query = $this->db->query($sql);
             $result = $query->result();
