ec947bb4246d3e37e4e179187d1d0b7c9c7794d1
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-03 20:01:11 +0430

    import

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -668,12 +668,7 @@ function general_setting(){
                         $headers  .= 'MIME-Version: 1.0' . "\r\n";
                         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
                         $send = mail($to, $subject, $message, $headers);
-						if($send){
-						echo 'mail send';
-						}
-						else {
-						echo 'error';
-						}
+						
                     }
                 }
                 fclose($handle);
