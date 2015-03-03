9519a096e5b917e63d7a47d6ee4210c9d9984f80
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-03 20:18:49 +0430

    i

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -637,8 +637,8 @@ function general_setting(){
             /*$subject = sprintf3( $subject, array('sender_name'=> $profile_info["s_first_name"],
                               'project_name'=> $project_name
                            ));*/
-
-            $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
+			$body = '';
+            $body .= htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
             //$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1) );
 			
             if (($handle = fopen($destfile, "r")) !== FALSE) {
@@ -658,8 +658,7 @@ function general_setting(){
 						$add_mem_id = $this->db->insert_id();
 						$body .= sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id) );
 					}
-					
-					echo $body;exit;
+					echo $body;
                     for ($c=0; $c < 1; $c++) {
                         $to      = $data[$c];
                         $subject = $subject;
@@ -669,7 +668,7 @@ function general_setting(){
                             'X-Mailer: PHP/' . phpversion() . "\r\n";
                         $headers  .= 'MIME-Version: 1.0' . "\r\n";
                         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-                        $send = mail($to, $subject, $message, $headers);
+                        mail($to, $subject, $message, $headers);
                     }
                 }
                 fclose($handle);
