0d1a17961576c3b7491b2c4a9f45f617c541ed4e
Trisha Paul <trisha.p@acumensoft.info>
2015-03-04 14:05:18 +0530

    test mail

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -633,7 +633,7 @@ function general_setting(){
             $this->load->model('mail_contents_model');
             $mail_info = $this->mail_contents_model->get_by_name("church_community_invitation_mail");
 
-            $subject = htmlspecialchars_decode($mail_info['subject'], ENT_QUOTES);
+            $subject = 'test';
             /*$subject = sprintf3( $subject, array('sender_name'=> $profile_info["s_first_name"],
                               'project_name'=> $project_name
                            ));*/
