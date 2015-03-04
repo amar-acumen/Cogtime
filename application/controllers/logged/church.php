faf52aff7c60bd58ed56fbda00474661a71e51a4
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-04 13:09:59 +0430

    import

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -639,7 +639,7 @@ function general_setting(){
                            ));*/
             $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
 			
-			$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1') );
+			//$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1') );
             if (($handle = fopen($destfile, "r")) !== FALSE) {
                 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $num = count($data);
@@ -656,8 +656,8 @@ function general_setting(){
 						$this->db->insert('cg_church_member_invitation', $invite_mem_info);
 						//echo $this->db->last_query();
 						$add_mem_id = $this->db->insert_id();
-						
-						//$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id) );
+						echo $body;
+						$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id) );
 						
 					}
                     for ($c=0; $c < 1; $c++) {
