2613964a31e57422352752c737b5aaab82fd7285
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-04 13:25:43 +0430

    import

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -657,7 +657,7 @@ function general_setting(){
 						//echo $this->db->last_query();
 						$add_mem_id = $this->db->insert_id();
 						$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id) );
-						
+						echo $body;
 					}
                     for ($c=0; $c < 1; $c++) {
                         $to      = $data[$c];
