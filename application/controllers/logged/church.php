55f8044017f41ab579559eef6339047a7fe659b2
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-04 12:38:58 +0430

    import

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -660,9 +660,7 @@ function general_setting(){
 						$body = sprintf3( $body, array('churchurl'=> base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id) );
 						
 					}
-					echo $body;
                     for ($c=0; $c < 1; $c++) {
-					echo '===='.$c;
                         $to      = $data[$c];
                         $subject = $subject;
                         $message = $body;
