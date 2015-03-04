c8822acd982d3df4222445b5933363d98ef3d18c
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-04 13:44:01 +0430

    import

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -1591,9 +1591,9 @@ echo json_encode( array('success'=>'true'));
 					'X-Mailer: PHP/' . phpversion() . "\r\n";
 				$headers  .= 'MIME-Version: 1.0' . "\r\n";
 				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-				echo $body;
+				echo $body;exit;
 				mail($to, $subject, $message, $headers);
-				echo json_encode(array('success'=>true,'arr_messages'=>$arr_messages,'msg'=>'Mail sent successfully'));
+				//echo json_encode(array('success'=>true,'arr_messages'=>$arr_messages,'msg'=>'Mail sent successfully'));
 				}
 			  }
 			}
