175702b78215904937b2702d90d2dd2442d49855
Sudipta <webguruslogic@gmail.com>
2015-03-04 16:36:31 +0530

    CSS n JS Performance minify Tool

--- application/controllers/module_generate.php
+++ application/controllers/module_generate.php
@@ -122,7 +122,7 @@ class Module_generate extends CI_Controller {
                     'js/switch.js',
                     'js/animate-collapse.js',
                     //'js/jquery-ui-1.8.2.custom.min.js',
-                    'js/stepcarousel.js',
+                    //'js/stepcarousel.js',
                     //'js/frontend/logged/tweets/tweet_utilities.js',
                     //'js/frontend/logged/christian_news_js.js',
                     'js/tab.js',
