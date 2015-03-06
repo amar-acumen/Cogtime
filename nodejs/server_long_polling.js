9ec93fd7c96f7a1a9e3a754a25b66cc65344aa9a
Trisha Paul <trisha.p@acumensoft.info>
2015-03-06 14:09:58 +0530

    node js

--- nodejs/server_long_polling.js
+++ nodejs/server_long_polling.js
@@ -45,6 +45,7 @@ function processRequest(url_parts,res,cnt)
 										var cnt_new = (new Date()).getTime();
 										if(nonZeroFound)
 										{
+											console.log('1');
 											res.writeHead(200, {'Content-Type': 'text/plain'});
 											res.end("setUpdateStatus('"+JSON.stringify(rows)+"');");
 										}
