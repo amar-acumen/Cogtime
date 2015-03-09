c3fb4120bfb1ad9ce0aae715358d543d5c15011c
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-09 20:07:04 +0430

    read bible

--- js/search.js
+++ js/search.js
@@ -237,7 +237,7 @@ function showscroll()
 }
 function scrollToGivenPosition(elementId)
 {
-alert('lastElementTop');
+alert($(elementId).position());
 	var lastElementTop = ($(elementId).position().top)*1;
 	
 	$("#table_content").scrollTop(lastElementTop);
