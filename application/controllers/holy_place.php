f2f7222c100f7f0193374fc3ccfeb5c353ef8a41
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-10 13:35:47 +0430

    read bible

--- application/controllers/holy_place.php
+++ application/controllers/holy_place.php
@@ -321,7 +321,7 @@ class Holy_place extends Base_controller
 		
 		$wh	= "";
 		$data['bible_data']	= $this->holy_place_model->get_bible($wh,$page,$this->pagination_per_page,'');
-		
+		pr($data['bible_data']);
 		
 		$data['pagination_per_page'] = $this->pagination_per_page;
 		//pr($result);
