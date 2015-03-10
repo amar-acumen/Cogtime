fb8221fda881261c77630bb67a5491444445738f
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-10 20:11:29 +0430

    sprintf event

--- application/controllers/logged/my_events.php
+++ application/controllers/logged/my_events.php
@@ -579,9 +579,9 @@ class My_events extends Base_controller {
         $data = $this->data;
         $s_where = " AND e.dt_end_time < '" . date('Y-m-d') . "'";
         $result = $this->events_model->get_my_events($i_user_id, $s_where, intval($page), $this->pagination_per_page);
-        echo $this->db->last_query();
+        //echo $this->db->last_query();
         $total_rows = $this->events_model->get_total_my_events($i_user_id, $s_where);
-        pr($result,1);
+        //pr($result,1);
         $data['arr_events'] = $result;
         $data['no_of_result'] = $total_rows;
         $data['current_page_1'] = $cur_page;
