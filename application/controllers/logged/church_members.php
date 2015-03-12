983a5a467d56c7bdf94fe83cc529392f6e3f991e
amar-acumen <amar@acumensoft.info>
2015-03-12 19:35:30 +0530

    test

--- application/controllers/logged/church_members.php
+++ application/controllers/logged/church_members.php
@@ -30,12 +30,7 @@ class Church_members extends Base_controller
             parent::_add_church_css_arr (array('css/church.css'));
             parent::check_login(TRUE, '', array('1')); // put this code on those pages which are not accessable by guest user
             # loading reqired model & helpers... 
-//			$user_id = intval(decrypt($this->session->userdata('user_id')));
-//                        parent::check_is_church_admin($user_id);
-            $this->load->model('users_model');
-			$this->load->model('user_notifications_model');
-            $this->load->model('user_alert_model');
-			$this->load->model('landing_page_cms_model');
+
             $this->load->model('church_new_model');
 			$this->load->helper('my_utility_helper');
 			$this->i_profile_id = intval(decrypt($this->session->userdata('user_id')));
