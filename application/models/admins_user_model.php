47509438cf4da5d65a24dc8d45104ee291614887
arifh <arif.zisu@gmail.com>
2015-03-12 20:51:51 +0530

    implements InfModel

--- application/models/admins_user_model.php
+++ application/models/admins_user_model.php
@@ -21,7 +21,7 @@
 
 include_once(APPPATH . 'models/base_model.php');
 
-class Admins_user_model extends Base_model implements InfModel {
+class Admins_user_model extends Base_model  {
 
     private $tbl_name;
     private $user_status;
