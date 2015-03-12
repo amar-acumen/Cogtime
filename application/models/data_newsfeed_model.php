78b6f14edbb657643c11d9514787ca84b42df1b7
arifh <arif.zisu@gmail.com>
2015-03-12 13:54:42 +0530

    testing....

--- application/models/data_newsfeed_model.php
+++ application/models/data_newsfeed_model.php
@@ -464,7 +464,7 @@ return $result_arr;
 
 }
 function get_photo_post_by_id($i_user_id,$s_where,$i_start_limit='', $i_no_of_page=''){
-    $i_start_limit = ($i_start_limit != 0 || $i_start_limit < 0 ) ? $i_start_limit : 0;
+   echo $i_start_limit = ($i_start_limit != 0 || $i_start_limit < 0 ) ? $i_start_limit : 0;
     
     
     
