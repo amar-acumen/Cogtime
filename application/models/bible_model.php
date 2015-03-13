919edf40b7b491c6b74f33e8bdbf9bb4db6fa402
amar-acumen <amar@acumensoft.info>
2015-03-13 20:00:26 +0530

    test

--- application/models/bible_model.php
+++ application/models/bible_model.php
@@ -11,7 +11,7 @@ class Bible_model extends Base_model
 	
 	public function get_by_id($id, $start_limit="", $no_of_page="") {
 		
-		$sql = sprintf('SELECT * FROM '.$this->db->BIBLE_QUIZ.'  where id = %s',  $id);
+		$sql = 'SELECT * FROM '.$this->db->BIBLE_QUIZ.'  where id ="'.$id.'"';
 		
 		$query = $this->db->query($sql); #echo $this->db->last_query(); exit;
 		$result_arr = $query->result_array();
@@ -40,7 +40,7 @@ class Bible_model extends Base_model
 	
 
 	public function delete_by_id($id) {
-	    $sql = sprintf( 'DELETE FROM '.$this->db->BIBLE_QUIZ.' WHERE id=%s', $id );
+	    $sql = 'DELETE FROM '.$this->db->BIBLE_QUIZ.' WHERE id="'.$id.'"';
 		$this->db->query($sql);
 		
 	}
