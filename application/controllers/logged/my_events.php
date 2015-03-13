3426396f9e40213c562553093716e8660ca9bef0
arifh <arif.zisu@gmail.com>
2015-03-13 17:42:18 +0530

    test my event

--- application/controllers/logged/my_events.php
+++ application/controllers/logged/my_events.php
@@ -97,10 +97,10 @@ class My_events extends Base_controller {
         $cur_time = date('Y-m-d');
         $s_where = " AND e.dt_end_time >= '" . $cur_time . "'";
         $result = $this->events_model->get_my_events($i_user_id, $s_where, intval($page), $this->pagination_per_page);
-        
+        pr($result);
       $total_rows = $this->events_model->get_total_my_events($i_user_id, $s_where);
      
-        
+        echo $total_rows ; 
         $data['arr_events'] = $result;
         $data['no_of_result'] = $total_rows;
         $data['current_page_1'] = $cur_page;
