09a2ec1631b2698c20da9f44f9c4dcc31fd67590
arifh <arif.zisu@gmail.com>
2015-03-13 15:45:53 +0530

    test

--- application/controllers/logged/my_events.php
+++ application/controllers/logged/my_events.php
@@ -98,7 +98,7 @@ class My_events extends Base_controller {
         $s_where = " AND e.dt_end_time >= '" . $cur_time . "'";
         $result = $this->events_model->get_my_events($i_user_id, $s_where, intval($page), $this->pagination_per_page);
         //echo $this->db->last_query();
-        $total_rows = $this->events_model->get_total_my_events($i_user_id, $s_where);
+      echo   $total_rows = $this->events_model->get_total_my_events($i_user_id, $s_where);
         //pr($result,1);
         $data['arr_events'] = $result;
         $data['no_of_result'] = $total_rows;
