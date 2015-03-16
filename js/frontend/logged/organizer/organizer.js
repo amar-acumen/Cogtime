6552a99e5441700f34f8a15950b07275079ee1d2
sanhita <sanhita@acumensoft.info>
2015-03-16 16:46:39 +0530

    test

--- js/frontend/logged/organizer/organizer.js
+++ js/frontend/logged/organizer/organizer.js
@@ -700,7 +700,7 @@ function showAddList(adddate,caldate){
 
 
 //function gotoDay(val_date){
-function gotoDay(day,month,year){
+function gotoDay(year,month,day){
 	/*page refresh*/
 	//$('#goto_date').val(val_date);
 	//$('#gotoFrm').submit();
