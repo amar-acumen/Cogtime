9c4cc7929ac587f72525f910f8f109ce0875dd25
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-17 17:43:58 +0430

    sprintf

--- application/models/tweet_model.php
+++ application/models/tweet_model.php
@@ -447,9 +447,7 @@ public function get_my_tweets($i_user_id, $s_where, $i_start_limit='', $i_no_of_
 					WHERE u.i_status='1' AND u.i_isdeleted ='1' AND t.i_isenabled =1 
 					 AND t.i_user_id = u.id AND t.i_user_id = '".intval($i_user_id)."' {$s_where}
 					ORDER BY dt_created_on DESC
-					"
-				, $this->db->dbprefix, intval($i_user_id), $s_where
-			);
+					";
 		}
 		else {
 		
@@ -492,18 +490,16 @@ public function get_my_tweets($i_user_id, $s_where, $i_start_limit='', $i_no_of_
 	public function get_total_my_tweets($i_user_id,  $s_where) {
 		
 
-		 $sql = sprintf("
+		 $sql = "
 				SELECT COUNT(*) count FROM (
 					(SELECT t.id 
 					  FROM cg_users u, cg_user_tweets t
 					  WHERE u.i_status='1' AND u.i_isdeleted ='1' AND t.i_isenabled =1 
-					  AND t.i_user_id = u.id AND t.i_user_id = %2\$s %3\$s)
+					  AND t.i_user_id = u.id AND t.i_user_id = '".intval($i_user_id)."' {$s_where})
 
 				
 				) derived_tbl
-					"
-				, $this->db->dbprefix, intval($i_user_id),$s_where
-			);
+					";
 		
 #and t.i_user_id != '%2\$s'
 		$query = $this->db->query($sql); //echo "sql ==>". nl2br($sql) ."<br />";  
@@ -540,7 +536,7 @@ public function get_my_tweets($i_user_id, $s_where, $i_start_limit='', $i_no_of_
 
 	public function delete_by_id($id) {
 	
-	     $sql = sprintf( 'DELETE FROM '.$this->db->USER_TWEETS.' WHERE id=%s', $id );
+	     $sql = 'DELETE FROM '.$this->db->USER_TWEETS.' WHERE id="'.$id.'"';
 		 $this->db->query($sql);
 		#echo $this->db->last_query(); exit;
 	}
@@ -590,7 +586,7 @@ public function get_my_tweets($i_user_id, $s_where, $i_start_limit='', $i_no_of_
 	
 	public function get_owner_by_tweet_id($id) {
 		
-		$sql = sprintf('SELECT i_user_id as i_owner_id  FROM '.$this->db->USER_TWEETS.'  WHERE id = %s ',$id);
+		$sql = 'SELECT i_user_id as i_owner_id  FROM '.$this->db->USER_TWEETS.'  WHERE id = "'.$id.'" ';
 		$query = $this->db->query($sql);
 		$result_arr = $query->result_array();
 		return $result_arr[0];
