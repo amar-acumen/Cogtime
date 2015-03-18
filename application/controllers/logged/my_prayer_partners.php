e7b2e6669c79d578a6f7c403fa08743f9de65c30
amar-acumen <amar@acumensoft.info>
2015-03-18 16:47:44 +0530

    test

--- application/controllers/logged/my_prayer_partners.php
+++ application/controllers/logged/my_prayer_partners.php
@@ -113,7 +113,7 @@ class My_prayer_partners extends Base_controller {
             ### search condition ###
             if (isset($_POST['search_basic']) && $_POST['search_basic'] == 'Y') {
                
-                echo $sql_churchmember = "SELECT member_id AS chid FROM cg_church_member WHERE is_approved=1 AND member_id='".$i_profile_id."'";
+                echo '========='.$sql_churchmember = "SELECT member_id AS chid FROM cg_church_member WHERE is_approved=1 AND member_id='".$i_profile_id."'";
                 exit;
                 $query_churchmember = $this->db->query($sql_churchmember);
                 $numrowmember = $query_churchmember->num_rows();
