cbec3cb8c59d8ca6c7dea118448c6610a04773b3
amar-acumen <amar@acumensoft.info>
2015-03-18 18:14:07 +0530

    test

--- application/controllers/logged/my_prayer_partners.php
+++ application/controllers/logged/my_prayer_partners.php
@@ -510,14 +510,14 @@ class My_prayer_partners extends Base_controller {
 
             if (isset($_POST['search_basic']) && $_POST['search_basic'] == 'Y') {
 
-                $sql_churchmember = "SELECT member_id AS members FROM cg_church_member WHERE church_id IN
+                $sql_churchmember = "SELECT member_id  FROM cg_church_member WHERE church_id IN
                         (SELECT church_id AS chid FROM cg_church_member WHERE is_approved=1 AND member_id='".$i_profile_id."') AND member_id!='".$i_profile_id."'";
                
                 
                 $query_churchmember = $this->db->query($sql_churchmember);
                 $result_churchmember = $query_churchmember->result_array();
                 pr($result_churchmember);
-                $query = "SELECT member_id AS members FROM cg_church_member WHERE church_id IN
+                $query = "SELECT member_id FROM cg_church_member WHERE church_id IN
                         (SELECT church_id AS chid FROM cg_church_admin WHERE is_approved=1 AND ch_admin_id='".$i_profile_id."') AND member_id!='".$i_profile_id."'";
                 $query_churchadmin = $this->db->query($query);
                 $result_churchadmin = $query_churchadmin->result_array();
