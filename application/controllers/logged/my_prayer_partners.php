c6d316e8f2762711e02a940253c5b1c440e1dc8b
amar-acumen <amar@acumensoft.info>
2015-03-18 21:05:37 +0530

    test

--- application/controllers/logged/my_prayer_partners.php
+++ application/controllers/logged/my_prayer_partners.php
@@ -527,7 +527,7 @@ class My_prayer_partners extends Base_controller {
                 $arrmember1 = array_diff($arr_churchmember, $arr_churchadmin);
                 $arrmember2 = array_diff($arr_churchadmin, $arr_churchmember);
                 $member = implode(",",array_merge($arrmember1,$arrmember2));
-                $member = substr($member,0,-1)
+                $member = substr($member,0,-1);
 
 
                 $WHERE_COND = "";
