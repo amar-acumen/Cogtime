5647d3a9a8903678cac71ffb5c807b57c37b2f9a
arifh <arif.zisu@gmail.com>
2015-03-19 16:53:17 +0530

    test

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -654,44 +654,44 @@ function general_setting(){
 						$this->db->insert('cg_church_member_invitation', $invite_mem_info);
                                                 $add_mem_id = $this->db->insert_id();
 						
-                                                echo $data[0].'===';
-//                                                $to      = $data[0];
-//                        /*************check already cogtime user*******************/
-//                                               
-//                                                $query = $this->db->get_where('cg_users', array('s_email' => $data[0] ,'i_status' => 1));
-//                                                $result = $query->result();
-//                                            
-//                                                 //echo count($result);
-//                                                        
-//                                                  if(count($result) == 1){
-//                                                       $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
-//                                                      foreach ($query->result() as $row)
-//                                                        {
-//                                                          $user_id =  $row->id;
-//                                                        } 
-//                                                   $location =  base_url().'already_user/'.$_SESSION['logged_church_id'].'/1/'.$user_id.'/'.$add_mem_id;
-//                                                  $body = sprintf3( $body, array('churchurl'=> $location) );
-//                                                    
-//                                                }else if(count($result) == 0) {
-//                                                     $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
-//                                                   
-//                                                   $location =  base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id;
-//                                                   $body = sprintf3( $body, array('churchurl'=> $location) );
-//                                                }
-//                                                /***************************************************/
-//                        
-//                        
-//                       
-//						//echo $to;
-//                        $subject = $subject;
-//                       $message = $body;
-//                        $headers = 'From: admin@cogtime.com' . "\r\n" .
-//                            'Reply-To: admin@cogtime.com' . "\r\n" .
-//                            'X-Mailer: PHP/' . phpversion() . "\r\n";
-//                        $headers  .= 'MIME-Version: 1.0' . "\r\n";
-//                        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-//                        mail($to, $subject, $message, $headers);
-//                                                
+                                                echo $data[1].'===';
+                                                $to      = $data[1];
+                        /*************check already cogtime user*******************/
+                                               
+                                                $query = $this->db->get_where('cg_users', array('s_email' => $data[1] ,'i_status' => 1));
+                                                $result = $query->result();
+                                            
+                                                 //echo count($result);
+                                                        
+                                                  if(count($result) == 1){
+                                                       $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
+                                                      foreach ($query->result() as $row)
+                                                        {
+                                                          $user_id =  $row->id;
+                                                        } 
+                                                   $location =  base_url().'already_user/'.$_SESSION['logged_church_id'].'/1/'.$user_id.'/'.$add_mem_id;
+                                                  $body = sprintf3( $body, array('churchurl'=> $location) );
+                                                    
+                                                }else if(count($result) == 0) {
+                                                     $body = htmlspecialchars_decode($mail_info['body'], ENT_QUOTES);
+                                                   
+                                                   $location =  base_url().'church_registration_by_email/'.$_SESSION['logged_church_id'].'/1/'.$add_mem_id;
+                                                   $body = sprintf3( $body, array('churchurl'=> $location) );
+                                                }
+                                                /***************************************************/
+                        
+                        
+                       
+						//echo $to;
+                        $subject = $subject;
+                       $message = $body;
+                        $headers = 'From: admin@cogtime.com' . "\r\n" .
+                            'Reply-To: admin@cogtime.com' . "\r\n" .
+                            'X-Mailer: PHP/' . phpversion() . "\r\n";
+                        $headers  .= 'MIME-Version: 1.0' . "\r\n";
+                        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                        mail($to, $subject, $message, $headers);
+                                                
                                                 
                                                 
                                                 
