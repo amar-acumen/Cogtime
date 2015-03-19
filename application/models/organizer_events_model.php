ff12bdc237f6119612c406ca1c76c7c367a2bf6f
moinak <moinak@acumensoft.info>
2015-03-19 19:37:15 +0530

    query prob

--- application/models/organizer_events_model.php
+++ application/models/organizer_events_model.php
@@ -140,7 +140,7 @@ class Organizer_events_model extends Base_model
 			echo $sql = 	"SELECT * FROM ".$this->db->USER_PHOTOS
 				  		."  where  i_photo_album_id = '".$album_id."' AND i_user_id = '".$user_id."' ORDER BY i_order DESC LIMIT (".$start_limit.", ".$no_of_page.")";
 		}
-				
+		
 		$query = $this->db->query($sql); 
 			#echo $this->db->last_query(); 
 		$result_arr = $query->result_array();
