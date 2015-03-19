8243a1c96f77b17b87a769c608a4ca45021ef8c4
sidhisankalp <sidhisankalp1990@gmail.com>
2015-03-19 14:12:44 +0430

    sprintf

--- application/models/prayer_group_model.php
+++ application/models/prayer_group_model.php
@@ -974,12 +974,10 @@ class Prayer_group_model extends Base_model {
 						  WHERE u.i_status='1' AND u.i_isdeleted ='1' AND pg.i_isenabled = 1 
 						  AND pg_mem.i_user_id = '".intval($i_user_id)."' AND pg_mem.i_request = '0' 
 						  AND pg_mem.s_status = 'pending' 
-						   {$s_where}
+						   ".$s_where."
 						   group by pg_mem.id 
 					  
-					"
-                , $this->db->dbprefix, intval($i_user_id), $s_where
-        );
+					";
         //echo nl2br($sql); exit;
         $query = $this->db->query($sql);
         $result_arr = $query->result_array(); //pr($result_arr,1);
