12520a44cd576c19d214493a521f78c7d7ddc310
arifh <arif.zisu@gmail.com>
2015-03-23 12:53:10 +0530

    church

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1966,12 +1966,12 @@ class Users_model extends Base_model  {
         return $result_arr[0]['count'];
     }
     function fetch_church_admin($user_id){
-       $SQL = "SELECT id,s_name,dt_join_on FROM cg_church WHERE ch_admin_id='" . $user_id . "' AND i_disabled=1";  
+       $SQL = "SELECT id as admin_church_id ,s_name as admin_church_name,dt_join_on as admin_join_date FROM cg_church WHERE ch_admin_id='" . $user_id . "' AND i_disabled=1";  
           $result_arr = $this->db->query($SQL)->result_array();
             return $result_arr[0];
     }
     function fetch_church_member($user_id){
-        $SQL = "SELECT c.s_name , cm.church_id,cm.created_date FROM cg_church_member  cm, cg_church c  WHERE cm.member_id='" . $user_id . "' AND c.id = cm.church_id "; 
+        $SQL = "SELECT c.s_name as member_church_name , cm.church_id as member_church_id ,cm.created_date as member_join_date FROM cg_church_member  cm, cg_church c  WHERE cm.member_id='" . $user_id . "' AND c.id = cm.church_id "; 
    $result_arr = $this->db->query($SQL)->result_array();
             return $result_arr[0];
         }
