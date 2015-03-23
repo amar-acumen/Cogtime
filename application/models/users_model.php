ff486f4a94d28f2523876c6ab0dfcfd21f7c5095
arifh <arif.zisu@gmail.com>
2015-03-23 13:02:11 +0530

    church

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1971,7 +1971,7 @@ class Users_model extends Base_model  {
             return $result_arr[0];
     }
     function fetch_church_member($user_id){
-        $SQL = "SELECT c.s_name as member_church_name , cm.church_id as member_church_id ,cm.created_date as member_join_date ,cm.is_blocked as member_block_status FROM cg_church_member  cm, cg_church c  WHERE cm.member_id='" . $user_id . "' AND c.id = cm.church_id "; 
+        $SQL = "SELECT c.s_name as member_church_name , cm.church_id as member_church_id ,cm.created_date as member_join_date ,cm.is_blocked as member_block_status FROM cg_church_member  cm, cg_church c  WHERE cm.member_id='" . $user_id . "' AND c.id = cm.church_id AND cm.is_approved=1  "; 
    $result_arr = $this->db->query($SQL)->result();
             return $result_arr;
         }
