5a6a742e06a8f00528a9c2fa72960d439b04ba4b
arifh <arif.zisu@gmail.com>
2015-03-25 16:20:49 +0530

    test

--- application/models/contacts_model.php
+++ application/models/contacts_model.php
@@ -932,7 +932,7 @@ class Contacts_model extends Base_model
 	}
    public function fetch_contacts($uid)
    {
-	   $s_qry = "SELECT * FROM {$this->db->contacts} 
+	 echo  $s_qry = "SELECT * FROM {$this->db->contacts} 
 						WHERE i_user_id='".$uid."'"; 
 	   $rs=$this->db->query($s_qry)->result_array();
 	   return $rs;
