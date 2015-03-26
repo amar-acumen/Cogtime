9bdc6a410d50af507efa02f2554dfeb894f832c2
moinak <moinak@acumensoft.info>
2015-03-26 17:52:24 +0530

    gotodate

--- js/frontend/logged/organizer/organizer.js
+++ js/frontend/logged/organizer/organizer.js
@@ -699,8 +699,8 @@ function showAddList(adddate,caldate){
 }
 
 
-//function gotoDay(val_date){
-function gotoDay(year,day,month){
+function gotoDay(val_date){
+//function gotoDay(year,day,month){
 	/*page refresh*/
 	$('#goto_date').val(val_date);
 	$('#gotoFrm').submit();
