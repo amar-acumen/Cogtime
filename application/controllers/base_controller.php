b2b07cbba0aac1be8192a35655b88903a4aec84c
arifh <arif.zisu@gmail.com>
2015-03-30 16:14:37 +0530

    test

--- application/controllers/base_controller.php
+++ application/controllers/base_controller.php
@@ -5048,6 +5048,9 @@ $this->db->update('cg_church', $data);
           //  $query = $this->db->query('Select * from cg_church where s_name like "%'.$txt_name_church.'%" AND s_address like "%'.$txt_address_church.'%" AND s_postcode like "%'.$txt_postcode_church.'%" ' );
         $query =  $this->db->query('Select * from cg_church where s_name like "%'.$txt_name_church.'%" AND s_address like "%'.$txt_address_church.'%" AND s_postcode like "%'.$txt_postcode_church.'%" ');
             $result = $query->result();
+             $data['result_arr'] = $result;
+            $VIEW_FILE = 'church_list_ajax.phtml';
+            echo $this->load->view($VIEW_FILE, $data,true);
            
         }
         
