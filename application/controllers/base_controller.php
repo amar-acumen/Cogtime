0ffa33453b9d3d5f94161b35ef1519c76689a51c
arifh <arif.zisu@gmail.com>
2015-03-30 15:28:13 +0530

    test

--- application/controllers/base_controller.php
+++ application/controllers/base_controller.php
@@ -5079,10 +5079,11 @@ $this->db->update('cg_church_member', $data, array('church_id' => $church_id ,'m
 
         
          function church_advace_search(){  
+             
             $txt_name_church = $this->input->post('txt_name_church');
             $txt_address_church = $this->input->post('txt_address_church');
             $txt_postcode_church = $this->input->post('txt_postcode_church');
-            $query = $this->db->query('select * from cg_church where s_name like "%"'.$txt_name_church.'"%" AND s_address like "%"'.$txt_name_church.'"%" AND s_postcode like "%"'.$txt_name_church.'"%"  ');
+            $query = $this->db->query("select * from cg_church where s_name like '%".$txt_name_church."%' AND s_address like '%".$txt_address_church."%' AND s_postcode like '%".$txt_postcode_church."%'  ");
            echo $query;           exit();
             $result = $query->result();
             $data['result_arr'] = $result;
