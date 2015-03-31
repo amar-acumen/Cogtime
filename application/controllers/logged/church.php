d9469182b7ab51ed2dc0139107f464fcfe78ffb3
arifh <arif.zisu@gmail.com>
2015-03-31 12:47:17 +0530

    test

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -1175,7 +1175,7 @@ $this->email->message("$body");
     }
 
     function church_addsubadmin(){
-pr($_POST,1);
+//pr($_POST,1);
         $c_id = $_SESSION['logged_church_id'];
         $user_id = intval(decrypt($this->session->userdata('user_id')));
                         parent::check_is_church_admin($user_id,$c_id);
@@ -1202,7 +1202,7 @@ pr($_POST,1);
                            'role' => 2,
                            'access' => $accessstring
                            );
-               
+               pr($data,1);
                 $this->db->where('id', $_POST['memberid']);
                 $res = $this->db->update('cg_church_member', $data);
                 header('location:'.base_url().'church-subadmin'); 
