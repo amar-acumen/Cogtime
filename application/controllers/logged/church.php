29c596dd7a22149971b881bb321303a14dcf1419
sidhisankalp <sidhisankalp1990@gmail.com>
2015-04-07 13:26:07 +0430

    cogtime user checkbox

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -727,7 +727,33 @@ function general_setting(){
 	 public function cogtime_user_sent_invitation()
     {
         parent::check_login(TRUE, '', array('1'));
-	
+		$row = 1;
+		pr($this->input->post('cogtime_user_mail'));
+	    while ($data = $this->input->post('cogtime_user_mail'))  {
+		$row++;
+                 
+					if($row>2)
+					{
+					
+					echo '======'.$data[0];
+						/****************already member***********************/
+						/**********if already invited member**********************************/
+						$query1 = $this->db->get_where('cg_church_member_invitation', array('email' => $data[0] , 'church_id'=>$_SESSION['logged_church_id'] ));
+						$result = $query1->result();
+						
+						 $query2 = $this->db->get_where('cg_church', array('ch_admin_id' => get_user_id_byemail($data[0]) , 'id' =>$_SESSION['logged_church_id']));
+						  $result1 = $query2->result();
+						
+						if(count($result) > 0 || count($result1) > 0){
+							 continue;
+						}
+						/*******************************************/
+					
+					
+					}
+		
+		
+		}
 	
 	}
      
