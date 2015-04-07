b64b9fefd19bef03a65a700d1c9e07e96a1af7f2
arifh <arif.zisu@gmail.com>
2015-04-07 20:27:13 +0530

    test

--- application/controllers/logged/church.php
+++ application/controllers/logged/church.php
@@ -245,13 +245,13 @@ class Church extends Base_controller
             ## seacrh conditions : filter ############
             
             if($this->input->post('mem_nm')){
-                $s_where .= 'AND CONCAT(u.s_first_name,u.s_last_name) LIKE "%'.$this->input->post('mem_nm').'%"';
+                $s_where .= ' AND CONCAT(u.s_first_name,u.s_last_name) LIKE "%'.$this->input->post('mem_nm').'%"';
             }
             if($this->input->post('membr_eml')){
-               $s_where .='AND u.s_email LIKE "%'.$this->input->post('membr_eml').'%"'; 
+               $s_where .=' AND u.s_email LIKE "%'.$this->input->post('membr_eml').'%"'; 
             }
             if(!$this->input->post('mem_nm') && !$this->input->post('membr_eml')){
-                $s_where .= 'AND 1';
+                $s_where .= ' AND 1';
             }
             $c_id = $_SESSION['logged_church_id'];
             $order_by = " u.id DESC ";
