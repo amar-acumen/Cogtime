6909d00abe4acbb6a219c81261af2dd0e73a7331
sidhisankalp <sidhisankalp1990@gmail.com>
2015-04-07 18:28:25 +0430

    is frnd

--- application/helpers/my_utility_helper.php
+++ application/helpers/my_utility_helper.php
@@ -5273,7 +5273,7 @@ function get_subadmin_access($user_id , $cid){
 
 function is_friend($login_id , $profile_id) {
 	$ci = & get_instance();
-	$sql_frnd = "select s_status from cg_user_contacts where i_requester_id = '".$login_id."' AND i_accepter_id = '".$profile_id."'";
+	$sql_frnd = "select s_status from cg_user_contacts where i_requester_id = '".$login_id."' AND i_accepter_id = '151'";
 	echo $sql_frnd;
 	$query_check_friend = $ci->db->query($sql_frnd);
 	$result_check_friend = $query_check_friend->result();
