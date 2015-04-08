954111a15ba4896ae17a5c54c0cbffdfa40d96e5
sidhisankalp <sidhisankalp1990@gmail.com>
2015-04-08 15:15:34 +0430

    helper

--- application/helpers/my_utility_helper.php
+++ application/helpers/my_utility_helper.php
@@ -5307,7 +5307,7 @@ function is_friend($login_id, $profile_id) {
 	//echo $sql_frnd;					
 	$query_check_friend = $ci->db->query($sql_frnd);
 	$result_check_friend = $query_check_friend->result();
-	pr($result_check_friend);
+	//pr($result_check_friend);
 	if (!empty($result_check_friend)) {
 	$status = $result_check_friend[0]->s_status;
 	}
