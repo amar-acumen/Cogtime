1310e9d59ec56c797181d7437da9aedcd7da3138
arifh <arif.zisu@gmail.com>
2015-04-09 19:38:21 +0530

    test

--- application/controllers/admin/build_kingdom/churches.php
+++ application/controllers/admin/build_kingdom/churches.php
@@ -808,10 +808,14 @@ $order_by='`dt_created_on` DESC';
         $id = $this->input->post('id');
         $query = $this->db->query('select u.id,CONCAT(u.s_first_name,u.s_last_name) as user_name , u.s_profile_photo,u.e_gender from cg_users u , cg_church_member cm where cm.church_id='.$id.' AND cm.is_blocked = 1 AND cm.is_leave = 0 AND cm.member_id = u.id AND cm.is_approved = 1');
         $res = $query->result();
+        if(count($res) > 0){
         foreach ($res as $val){
             echo '<ul>';
             echo '<li> <img src="'.get_profile_image_of_user('thumb',$val->s_profile_photo,$val->e_gender).'" title='.$val->user_name.' ><span class="mem_nm">'.$val->user_name.'</span><li>';
             echo '</ul>';
+        }
+        }else{
+            echo '<h1>No Members</h1>';
         }
     }
 }   // end of controller...
