28b1e1cbc9226cb6c10096ee2511aa59a0bf5f06
arifh <arif.zisu@gmail.com>
2015-04-10 11:55:55 +0530

    test

--- application/controllers/admin/build_kingdom/churches.php
+++ application/controllers/admin/build_kingdom/churches.php
@@ -733,7 +733,7 @@ $this->email->message("$body");
 				$WHERE_COND .= ($srch_city=='0')?'':" AND ( C.i_city_id  =  '".$srch_city."')";
                                  $srch_city = intval(decrypt(trim($this->input->post('srch_city'))));
                                  
-                                 $srch_name = trim($this->input->post('church_name'));
+                                 $srch_name = preg_replace('/[^A-Za-z0-9\-]/', '', trim($this->input->post('church_name'))); 
 				$WHERE_COND .= ($srch_name=='0')?'':" AND ( C.s_name  LIKE '%".$srch_name."%')";
                                 if(trim($this->input->post('date_filter'))!= '')
                                 {
